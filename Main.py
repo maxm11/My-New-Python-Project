e42f33c2f715e572dc63a5253f7400e5c5951df9
maxmrphy@gmail.com <>
2013-07-09 05:57:46 +0000

    Edited Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,22 @@
 # Hello world python program
-print "Hello World!";
+print "Hello!";
+print "Welcome to Max's Tinkering Enviroment for Python."
+print "Just to get familiar with my good looking users..."
+name = rawinput("What is your name? : ")
+print "%f, you have the best name I have ever heard!" % name
+gender = rawinput("Are you a gentleman, or a fine lady? Enter 1 for gentleman or 2 for lady : ")
+
+if gender = "1":
+print "Ok, %f if you're looks were rated 1-10, would be a eww 1, or a devistatingly hansome 10?" % name
+elif gender = "2":
+print "Ok, %f if you're looks were rated 1-10, would be a eww 1, or a gorgeous 10?" % name
+else
+print "ERROR"
+number = rawinput("Enter Number : ")
+
+if number = "10"
+print "I agree!!"
+else
+print "Why a %f, I think you're a 10!" % number
+
+print "That's all I do."
