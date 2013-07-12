ad1a00b2d2e9297495e99075b71a76f1e6342bc5
Max M <maxmrphy@gmail.com>
2013-07-12 01:47:09 -0400

    Added a new Calculator script

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+print ("--------------------------------------")
+print (" ")
+
+print ("Calculator Python Program")
+print ("Made by Max, licensed under GPLv3")
+
+print (" ")
+print ("--------------------------------------")
+
+num1 = raw_input ("Please enter your first number here : ")
+
+
+sign = raw_input ("Please enter the operation you want to use [+ - * /]: ")
+
+num2 = raw_input ("Please enter your second number here : ")
+
+ans = 0
+
+def calculation( num1, num2, sign)
+{
+    if sign == "+":
+        num1 + num2 = ans
+    
+    if sign == "-":
+        num1 - num2 = ans
+    
+    if sign == "*":
+        num1 * num2 = ans
+    
+    if sign == "/":
+        num1 / num2 = ans
+}
