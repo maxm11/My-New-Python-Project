755e94046dd348843e727de6fd52befa307656bb
Max Murphy <maxmrphy@gmail.com>
2013-07-12 22:47:10 -0700

    Filtering for operations, in order to not allow for an error.

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+import sys
 print ("--------------------------------------")
 print (" ")
 
@@ -11,22 +12,28 @@ num1str = raw_input ("Please enter your first number here : ")
 
 sign = raw_input ("Please enter the operation you want to use [+ - * /]: ")
 
-num2str = raw_input ("Please enter your second number here : ")
+##Makes sure no invalid signs are being used to not break program.
+if  sign != "+" and sign != "-" and sign != "*" and sign != "/":
+    print ("ERROR, invalid operation used")
+    sys.exit()
+else:
 
-num1 = int (num1str)
-num2 = int (num2str)
+    num2str = raw_input ("Please enter your second number here : ")
+
+    num1 = int (num1str)
+    num2 = int (num2str)
     
-if sign == "+":
-    ans = num1 + num2
+    if sign == "+":
+        ans = num1 + num2
     
-if sign == "-":
-    ans = num1 - num2
+    if sign == "-":
+        ans = num1 - num2
     
-if sign == "*":
-    ans = num1 * num2
+    if sign == "*":
+        ans = num1 * num2
     
-if sign == "/":
-    ans = num1 / num2
+    if sign == "/":
+        ans = num1 / num2
     
 
-print ("The answer is : "), ans
+    print ("The answer is : "), ans
